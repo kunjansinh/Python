e3755e666b90ee5a97ef28ac0d05b2f3113a5071
kunjansinh <kunjansinh90@gmail.com>
2026-08-12 16:42:17 +0100

    Add files via upload

---
+++ Week-1/simple_calculator.py
@@ -0,0 +1,14 @@
+# Simple Calculator
+
+number_1 = 20
+number_2 = 5
+
+addition = number_1 + number_2
+subtraction = number_1 - number_2
+multiplication = number_1 * number_2
+division = number_1 / number_2
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
---
+++ Week-1/student_information.py
@@ -0,0 +1,11 @@
+# Student Information Program
+
+name = "Kunjan"
+age = 20
+course = "Computer Science"
+is_student = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Course: {course}")
+print(f"Student: {is_student}")
