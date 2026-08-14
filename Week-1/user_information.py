b14a47a85d7510327b1d00d6cf5702bf6e2b310f
kunjansinh <kunjansinh90@gmail.com>
2026-08-14 16:07:58 +0100

    Add files via upload

---
+++ Week-1/user_information.py
@@ -0,0 +1,8 @@
+# User Information Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+print(f"Next year, you will be {age + 1} years old.")
