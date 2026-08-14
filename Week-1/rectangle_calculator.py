04d652a15c509f4f093535c293fb10edb1e8a8f7
kunjansinh <kunjansinh90@gmail.com>
2026-08-14 16:08:27 +0100

    Add files via upload

---
+++ Week-1/rectangle_calculator.py
@@ -0,0 +1,10 @@
+# Rectangle Calculator
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
