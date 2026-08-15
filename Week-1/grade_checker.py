f228ff42bbbd5080f44863d78f435de09adb1125
kunjansinh <kunjansinh90@gmail.com>
2026-08-15 22:08:51 +0100

    Add files via upload

---
+++ Week-1/grade_checker.py
@@ -0,0 +1,14 @@
+# Grade Checker
+
+mark = float(input("Enter your mark: "))
+
+if mark >= 70:
+    print("Grade: A")
+elif mark >= 60:
+    print("Grade: B")
+elif mark >= 50:
+    print("Grade: C")
+elif mark >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
