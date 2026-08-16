cc17155e3c7eafa199f088fed99c5a69d86c7398
kunjansinh <kunjansinh90@gmail.com>
2026-08-16 17:09:06 +0100

    Add files via upload

---
+++ Week-1/student_marks.py
@@ -0,0 +1,12 @@
+# Student Marks Program
+
+marks = [65, 72, 48, 81, 56]
+
+print("Student Marks:")
+
+for mark in marks:
+    print(mark)
+
+average = sum(marks) / len(marks)
+
+print(f"Average Mark: {average}")
