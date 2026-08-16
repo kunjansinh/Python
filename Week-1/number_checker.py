e476ebe540d7d9490e212f4313fca472cb2306ea
kunjansinh <kunjansinh90@gmail.com>
2026-08-16 17:09:21 +0100

    Add files via upload

---
+++ Week-1/number_checker.py
@@ -0,0 +1,10 @@
+# Number Checker
+
+numbers = [3, 8, 12, 5, 20, 7]
+
+for number in numbers:
+
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
